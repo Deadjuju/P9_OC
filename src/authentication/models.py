5739c92abe82ba45efb87dface393c22b149a23a
Julien Garcia <julien.garcia-lp@laposte.net>
2022-05-10 15:46:13 +0200

    Affichage du nombre d'abonnés (avec pluralisation)

--- src/authentication/models.py
+++ src/authentication/models.py
@@ -6,5 +6,9 @@ class User(AbstractUser):
     
     profile_photo = models.ImageField(verbose_name='photo de profil')
 
+    @property
+    def number_of_subscribers(self):
+        return self.followed_by.all().count()
+
     def __str__(self):
         return f"{self.username}"
