a5b3e322e753b9cf03b54a1f658f79d9252bd8dc
Julien Garcia <julien.garcia-lp@laposte.net>
2022-05-11 22:54:07 +0200

    Ajout User dans admin

--- src/authentication/admin.py
+++ src/authentication/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from authentication.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "first_name", "last_name", )
+    fields = ("username", "email", ("first_name", "last_name",), "date_joined", )
+    search_fields = ['username', ]
+    search_help_text = "Pseudo de l'utilisateur"
+
+
+admin.site.register(User, UserAdmin)
