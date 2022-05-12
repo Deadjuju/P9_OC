2234d542039f16606c504db6676fa0beca1537da
Julien Garcia <julien.garcia-lp@laposte.net>
2022-05-12 22:20:18 +0200

    Rajout changement de mot de passe

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     ), name="login"),
     path('logout/', LogoutView.as_view(), name="logout"),
     path('signup/', authentication.views.signup, name="signup"),
+    path(
+        'change-password/',
+        authentication.views.PasswordChangeView.as_view(),
+        name="password_change"),
 ]
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth import login
+from django.contrib.auth.forms import PasswordChangeForm
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import FormView
 
 from authentication import forms
 
@@ -20,3 +23,20 @@ def signup(request):
     return render(request,
                   'authentication/signup.html',
                   context={'form': form})
+
+
+class PasswordChangeView(FormView):
+    template_name = 'authentication/password_change_form.html'
+    form_class = PasswordChangeForm
+    success_url = reverse_lazy('posts')
+
+    def get_form_kwargs(self):
+        kwargs = super(PasswordChangeView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
+
+    def form_valid(self, form):
+        form.save()
+        success_message = "Votre mot de passe a bien été changé."
+        messages.add_message(self.request, messages.SUCCESS, message=success_message)
+        return super(FormView, self).form_valid(form)
